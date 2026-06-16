6afb8a376239a6bd4a71be711b087efc9d62e7cc
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-16 11:30:45 +0100

    move due date calculation from DurationEnum to Subscription

--- src/ssum/model/duration_enum.py
+++ src/ssum/model/duration_enum.py
@@ -1,20 +1,6 @@
 from enum import Enum
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 
 class DurationEnum(Enum):
     WEEK = 1
     MONTH = 2
     YEAR = 3
-
-    def next_renewal(self) -> datetime:
-        '''Return the next time this subscription needs renewing.'''
-
-        now = datetime.now()
-
-        # Use the relativedelta class to account for leap years
-        # and differing amounts of days in a month.
-        match (self):
-            case DurationEnum.WEEK: return now + timedelta(weeks=1)
-            case DurationEnum.MONTH: return now + relativedelta(months=1)
-            case DurationEnum.YEAR: return now + relativedelta(years=1)
--- src/ssum/model/models/subscription.py
+++ src/ssum/model/models/subscription.py
@@ -1,5 +1,10 @@
+
+from datetime import datetime, timedelta
+
 from sqlalchemy import Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+from dateutil.relativedelta import relativedelta
 
 from ssum.model.db import db
 from ssum.model.duration_enum import DurationEnum
@@ -12,7 +17,7 @@ class Subscription(db.Model, TimestampMixin):
     id = mapped_column(Integer(), primary_key=True)
     name = mapped_column(String(length=255), nullable=False)
     price_in_pence = Mapped[int]
-    duration = Enum(DurationEnum) # The due date can be retrieved from the duration enum.
+    duration = Enum(DurationEnum)
 
     # Optional relationship to the preset it was defined with.
     preset_id = mapped_column(ForeignKey("subscription_preset.id"))
@@ -29,3 +34,16 @@ class Subscription(db.Model, TimestampMixin):
         "SubscriptionPresetTier", uselist=False,
         back_populates="used_by"
     )
+
+    @hybrid_property
+    def next_renewal(self) -> datetime:
+        '''Return the next time this subscription needs renewing.'''
+
+        now = datetime.now()
+
+        # Use the relativedelta class to account for leap years
+        # and differing amounts of days in a month.
+        match (self):
+            case DurationEnum.WEEK: return now + timedelta(weeks=1)
+            case DurationEnum.YEAR: return now + relativedelta(years=1)
+            case _: return now + relativedelta(months=1)
