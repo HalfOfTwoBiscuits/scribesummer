5aa601b3426ea45e812e6c4f5dd221ea0d2d9c91
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-16 11:35:16 +0100

    rename 'preset' file

--- src/ssum/model/models/subscription_preset.py
+++ src/ssum/model/models/subscription_preset.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer
+from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from ssum.model.db import db
@@ -7,7 +7,7 @@ from ssum.model.models.timestamp_mixin import TimestampMixin
 class SubscriptionPreset(db.Model, TimestampMixin):
     '''A pre-populated preset for a subscription of a certain brand.'''
 
-    id = mapped_column(Integer(), primary_key=True)
+    id = mapped_column(String(), primary_key=True)
     name = Mapped[str]
 
     # Available tiers for this preset.
