d07bad91db4d99e76a1c422dd5f744f66d274e2b
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-16 12:20:49 +0100

    add new base class for the two types of subscription

---
+++ src/ssum/model/models/base_subscription.py
@@ -0,0 +1,57 @@
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from sqlalchemy import Integer, ForeignKey, Enum
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.ext.hybrid import hybrid_method
+
+from ssum.model.db import db
+from ssum.model.duration_enum import DurationEnum
+from ssum.model.models.timestamp_mixin import TimestampMixin
+
+class Subscription(db.Model, TimestampMixin):
+    '''One of the user's paid subscriptions.
+    
+    This is the base class for
+    SubscriptionFromPreset and CustomSubscription.
+
+    Unlike those, it is a table in the database.
+    This class is the table,
+    and the children specify specific attributes.'''
+
+    type = Mapped[str] # Indicates preset or custom.
+
+    # This class is used as a polymorphic base,
+    # meaning it is a table, but records are added only
+    # through the children rather than directly.
+    __mapper_args__ = {
+        "polymorphic_on": "type",
+        "polymorphic_abstract": True
+    }
+
+    first_renewal: Mapped[datetime]
+
+    @hybrid_method
+    def next_renewal(self, duration: DurationEnum) -> datetime:
+        '''Return the next time this subscription needs renewing.
+
+        The renewal duration is passed as an argument,
+        because depending on the child class it might be its own
+        attribute or be determined by the preset subscription tier.'''
+
+        # Starting from the first renewal,
+        # repeatedly add the renewal duration
+        # until the date is in the future.
+        now = datetime.now()
+        renewal = self.first_renewal
+        while renewal < now:
+            # Use the relativedelta class to account for leap years
+            # and differing amounts of days in a month.
+            match (duration.value):
+                case DurationEnum.WEEK: renewal += relativedelta(weeks=1)
+                case DurationEnum.YEAR: renewal += relativedelta(years=1)
+                case _: renewal += relativedelta(months=1)
+
+        return renewal
+        
