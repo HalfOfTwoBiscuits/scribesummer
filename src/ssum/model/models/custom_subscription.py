2c685d303f5e2f8a4675c5a1b823bcd032adc944
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-16 14:06:07 +0100

    separate custom and preset-based subscriptions

---
+++ src/ssum/model/models/custom_subscription.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Integer, Enum
+from sqlalchemy.orm import Mapped, relationship
+
+from ssum.model.db import db
+from ssum.model.interval_enum import IntervalEnum
+from ssum.model.models.base_subscription import Subscription
+from ssum.model.models.category_joins import category_for_custom_sub
+
+class CustomSubscription(Subscription):
+    '''One of the user's paid subscriptions.
+    Its attributes were specified manually by the user,
+    without using a preset.
+    
+    This isn't a table in the database, its parent Subscription is.
+    This class just specifies that the attributes are set directly,
+    rather than using a relationship to a preset.
+
+    SQLAlchemy calls this "Single Table Inheritance".
+    See: https://docs.sqlalchemy.org/en/20/orm/inheritance.html'''
+
+    name = Mapped[str]
+    price_in_pence = Mapped[int]
+    interval = Enum(IntervalEnum)
+
+    categories = relationship(
+        "Category",
+        secondary=category_for_custom_sub,
+        back_populates="custom_subscriptions_using_it"
+    )
+
+    # Its `type` attribute will be set to "custom".
+    __mapper_args__ = {
+        "polymorphic_identity": "custom"
+    }
---
+++ src/ssum/model/models/subscription_from_preset.py
@@ -0,0 +1,53 @@
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from ssum.model.models.base_subscription import Subscription
+
+class SubscriptionFromPreset(Subscription):
+    '''One of the user's paid subscriptions.
+    Its attributes are specified by a preset for a particular
+    company and subscription tier.
+
+    This isn't a table in the database, its parent Subscription is.
+    This class just specifies how the preset relationship works.
+    
+    QLAlchemy calls this "Single Table Inheritance".
+    See: https://docs.sqlalchemy.org/en/20/orm/inheritance.html'''
+
+    # Optional relationship to the preset it was defined with.
+    preset_id = mapped_column(ForeignKey("subscription_preset.id"))
+
+    # This Relationship object allows the preset to be easily retrieved in Python.
+    preset_obj = relationship(
+        "SubscriptionPreset", uselist=False,
+        back_populates="used_by"
+    )
+
+    # Also related to one of `SubscriptionPreset.tiers`.
+    tier_id = mapped_column(ForeignKey("subscription_preset_tier.id"))
+    tier_obj = relationship(
+        "SubscriptionPresetTier", uselist=False,
+        back_populates="used_by"
+    )
+
+    @hybrid_property
+    def name(self) -> str:
+        '''Name of the subscription as determined by its preset.'''
+        return self.tier_obj.name
+    
+    @hybrid_property
+    def price_in_pence(self) -> int:
+        '''Price in pence of this subscription as determined by its preset.'''
+        return self.tier_obj.price_in_pence
+
+    @hybrid_property
+    def interval(self) -> int:
+        '''Billing interval of this subscription as determined by its preset.'''
+        return self.tier_obj.billing_interval
+    
+    # Its `type` attribute will be set to "preset_based".
+    __mapper_args__ = {
+        "polymorphic_identity": "preset_based"
+    }
