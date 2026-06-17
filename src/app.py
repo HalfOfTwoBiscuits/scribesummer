fcbb5b3853b53096d2b8a987b89d9aacc8359edd
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-17 12:24:37 +0100

    add terminal command for population

--- src/app.py
+++ src/app.py
@@ -16,3 +16,14 @@ factory = AppFactory()
 
 # On the server WSGI script to deploy the app, 'application' is imported.
 app = application = factory.app
+
+@app.cli.command("populate")
+def populate():
+    '''Pre-populate the database with subscription
+    presets and categories from model/data/presets.json
+    and model/data/categories.json.
+    
+    If a preset or category already exists then it will be updated
+    to match the JSON data.'''
+
+    factory.model_handler.prepopulator.populate()
