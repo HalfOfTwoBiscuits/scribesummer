7f0ea69f8ba5817fdd073fada620fdb56a016acc
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-17 10:01:24 +0100

    use meaningful strings for interval enum values

--- src/ssum/model/interval_enum.py
+++ src/ssum/model/interval_enum.py
@@ -3,6 +3,6 @@ from enum import Enum
 class IntervalEnum(Enum):
     '''Enum for the billing interval of a subscription.'''
     
-    WEEK = 1
-    MONTH = 2
-    YEAR = 3
+    WEEK = "w"
+    MONTH = "m"
+    YEAR = "y"
