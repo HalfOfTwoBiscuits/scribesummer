545d535e17c3516d5358f5fffe18d27f42c73e12
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-06-18 09:57:03 +0100

    create subscription record on form submission

--- src/ssum/view/views/add_subscription.py
+++ src/ssum/view/views/add_subscription.py
@@ -2,6 +2,8 @@ from flask import render_template, redirect, url_for
 from flask.views import View
 from werkzeug.wrappers.response import Response
 
+from scribesummer.src.ssum.model.interval_enum import IntervalEnum
+
 class AddSubscription(View):
     methods = ["GET", "POST"]
 
@@ -16,10 +18,29 @@ class AddSubscription(View):
         cats = self.__mh.db.session.execute(
             self.__mh.db.select(CategoryModel)
         ).scalars()
-        cat_names = [cat.name for cat in cats]
+
+        cat_names = [(cat.id, cat.name) for cat in cats]
         form.categories.choices = cat_names
 
         if form.validate_on_submit():
+            CustomSubscription = self.__mh.get_model("CustomSubscription")
+            UserModel = self.__mh.get_model("User")
+            user = self.__mh.db.get_or_404(UserModel, 1)
+
+            # Get categories selected.
+            matching_cats = [cat for cat in cats if cat.id in form.categories.data]
+
+            sub = CustomSubscription(
+                name=form.name.data,
+                price_in_pence=int(form.price.data * 100),
+                interval=IntervalEnum(form.interval.data[0].lower()),
+                categories=matching_cats,
+                first_renewal=form.date.data,
+                user_obj=user
+            )
+            self.__mh.db.session.add(sub)
+            self.__mh.db.session.commit()
+
             return redirect(url_for("my_subscriptions"))
 
         return render_template("add_subscription.html", form=form)
